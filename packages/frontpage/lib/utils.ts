939ac1d8a4f96c5188593b439cf3795361d796d7
nandi <nandithebull@outlook.com>
2024-10-26 16:17:50 -0700

    add title automatically

--- packages/frontpage/lib/utils.ts
+++ packages/frontpage/lib/utils.ts
@@ -5,6 +5,13 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export async function grabTitle(url: string): Promise<string> {
+  url = encodeURI(url);
+  let response = await fetch(`https://cardyb.bsky.app/v1/extract?url=${url}`)
+  let body = await response.json()
+  return body.title
+}
+
 export type Prettify<T> = {
   [K in keyof T]: T[K];
   // eslint-disable-next-line @typescript-eslint/ban-types
